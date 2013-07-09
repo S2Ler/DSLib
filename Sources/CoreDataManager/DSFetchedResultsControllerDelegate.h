9bc42f3b0d0f972fe3142afe1d99ce1357c32260
Alexander Belyavskiy <diejmon@gmail.com>
2013-07-09 21:35:23 +0300

    added fetched results controller

---
+++ Sources/CoreDataManager/DSFetchedResultsControllerDelegate.h
@@ -0,0 +1,39 @@
+
+#import <Foundation/Foundation.h>
+
+@class DSFetchedResultsControllerDelegate;
+
+@protocol DSConfigurableTableController <NSObject>
+- (void)updateCellAtIndexPath:(NSIndexPath *)indexPath;
+
+- (UITableView *)tableView;
+
+@optional
+- (void)   fetchDelegate:(DSFetchedResultsControllerDelegate *)theDelegate
+didDeleteItemAtIndexPath:(NSIndexPath *)theIndexPath;
+
+- (void)   fetchDelegate:(DSFetchedResultsControllerDelegate *)theDelegate
+didMoveItemFromIndexPath:(NSIndexPath *)theOldIndexPath
+             toIndexPath:(NSIndexPath *)theNewIndexPath;
+
+- (void)   fetchDelegate:(DSFetchedResultsControllerDelegate *)theDelegate
+didInsertItemAtIndexPath:(NSIndexPath *)theIndexPath;
+
+- (void)fetchDelegateStartedUpdateTable:(DSFetchedResultsControllerDelegate *)theDelegate;
+- (void)fetchDelegateAboutToEndTableUpdate:(DSFetchedResultsControllerDelegate *)theDelegate;
+
+@end
+
+@interface DSFetchedResultsControllerDelegate : NSObject
+  <
+  NSFetchedResultsControllerDelegate
+  >
+{
+
+}
+
+@property (weak, nonatomic) id<DSConfigurableTableController> delegate;
+
+- (id)initWithTableViewController:(id<DSConfigurableTableController>)controller;
+
+@end
