9da68a41218bafae86181ab4673f0b7d7611975b
Alexander Belyavskiy <diejmon@gmail.com>
2013-12-11 23:32:28 +0300

    removed dependency resolver class as it the cause of the protocol resolving

--- Sources/Managers/IoSDependencyResolver/DependencyRegister.h
+++
@@ -1,9 +0,0 @@
-
-#import <Foundation/Foundation.h>
-
-
-@interface DependencyRegister : NSObject {
-    
-}
-+ (void)registerDependencies;
-@end
--- Sources/Managers/IoSDependencyResolver/DependencyResolver+Register.h
+++
@@ -1,8 +0,0 @@
-
-#import <Foundation/Foundation.h>
-#import "DependencyResolver.h"
-
-@interface DependencyResolver (Register)
-+ (void)addClass:(Class)theConcreteClass
-     forProtocol:(Protocol *)theProtocol;
-@end
