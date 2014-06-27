adaf0d14c4da36db92414756d6514237e4733c2d
Alexander Belyavskiy <diejmon@gmail.com>
2014-06-27 12:11:27 +0300

    email sharer

---
+++ Sort/DSEmailSharer.h
@@ -0,0 +1,29 @@
+//
+//  DSEmailSharer.h
+//  PropertyApp
+//
+//  Created by Alexander Belyavskiy on 2/10/12.
+//  Copyright (c) 2012 InGenius Labs. All rights reserved.
+//
+
+@import MessageUI;
+
+@interface DSEmailSharer : NSObject
+<
+MFMailComposeViewControllerDelegate
+>
+- (id)initWithRootViewController:(UIViewController *)theController;
+
+- (void)setSubject:(NSString*)theSubject;
+- (void)addRecipient:(NSString *)theRecipient;
+- (void)setMessageBody:(NSString *)theBody isHTML:(BOOL)theHTMLFlag;
+- (void)addAttachmentData:(NSData*)theAttachment
+                 mimeType:(NSString*)theMimeType
+                 fileName:(NSString*)theFilename;
+
+@property (nonatomic, copy) void (^completionHandler)();
+
+- (void)share;
+
++ (BOOL)canSendEmail;
+@end
---
+++ Sort/DSEmailSharer.m
@@ -0,0 +1,87 @@
+//
+//  DSEmailSharer.m
+//  PropertyApp
+//
+//  Created by Alexander Belyavskiy on 2/10/12.
+//  Copyright (c) 2012 InGenius Labs. All rights reserved.
+//
+
+#import "DSEmailSharer.h"
+
+#pragma mark - private
+@interface DSEmailSharer()
+@property (nonatomic, weak) UIViewController *rootViewController;
+@property (nonatomic, strong) MFMailComposeViewController *mailComposer;
+@property (nonatomic, strong) NSMutableArray *recipients;
+@end
+
+@implementation DSEmailSharer
+@synthesize rootViewController = _rootViewController;
+@synthesize mailComposer = _mailComposer;
+@synthesize recipients = _recipients;
+
+- (id)initWithRootViewController:(UIViewController *)theController
+{
+  self = [super init];
+  
+  if (self) {
+    _rootViewController = theController;    
+    _mailComposer = [[MFMailComposeViewController alloc] init];
+    [_mailComposer setMailComposeDelegate:self];
+    _recipients = [NSMutableArray array];
+  }
+  
+  return self;
+}
+
+- (void)share
+{
+  [[self mailComposer] setToRecipients:[self recipients]];
+  
+  [[self rootViewController] presentViewController:[self mailComposer] animated:YES completion:nil];
+ }
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller 
+          didFinishWithResult:(MFMailComposeResult)result
+                        error:(NSError *)error
+{
+  [[self rootViewController] dismissViewControllerAnimated:YES completion:nil];
+  
+  if ([self completionHandler]) {
+    [self completionHandler]();
+  }
+}
+
+- (void)setSubject:(NSString*)theSubject
+{
+  [[self mailComposer] setSubject:theSubject];
+}
+
+- (void)addRecipient:(NSString *)theRecipient
+{
+  if (theRecipient == nil) {
+    NSLog(@"Tring to add nil object");
+    return;
+  }
+  
+  [[self recipients] addObject:theRecipient];
+}
+
+- (void)setMessageBody:(NSString *)theBody
+                isHTML:(BOOL)theHTMLFlag
+{
+  [[self mailComposer] setMessageBody:theBody isHTML:theHTMLFlag];
+}
+
+- (void)addAttachmentData:(NSData*)theAttachment
+                 mimeType:(NSString*)theMimeType
+                 fileName:(NSString*)theFilename
+{
+  [[self mailComposer] addAttachmentData:theAttachment mimeType:theMimeType fileName:theFilename];
+}
+
++ (BOOL)canSendEmail
+{
+  return [MFMailComposeViewController canSendMail];
+}
+@end
