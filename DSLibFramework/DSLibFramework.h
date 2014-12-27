ce535236a2808de922908ee9c811bbc7c555dd77
Alexander Belyavskiy <diejmon@gmail.com>
2014-12-27 18:31:57 +0300

    Framework target added, submodules updated.

---
+++ DSLibFramework/DSLibFramework.h
@@ -0,0 +1,19 @@
+//
+//  DSLibFramework.h
+//  DSLibFramework
+//
+//  Created by Alexander Belyavskiy on 12/27/14.
+//  Copyright (c) 2014 DS ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DSLibFramework.
+FOUNDATION_EXPORT double DSLibFrameworkVersionNumber;
+
+//! Project version string for DSLibFramework.
+FOUNDATION_EXPORT const unsigned char DSLibFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSLibFramework/PublicHeader.h>
+
+
