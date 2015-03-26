bcc1008845ae960469464b8f6d534f130d382058
Alexander Belyavskiy <diejmon@gmail.com>
2015-03-26 16:36:46 +0300

    Possible fix when domains or errors are not strings

--- Sources/Alerts/DSAlertsSupportCode.m
+++ Sources/Alerts/DSAlertsSupportCode.m
@@ -3,9 +3,9 @@
 
 BOOL DSMessageDomainsEqual(DSMessageDomain *domain1, DSMessageDomain *domain2)
 {
-  return [domain1 isEqualToString:domain2];
+  return [domain1 isEqual:domain2];
 }
 
 BOOL DSMessageCodesEqual(DSMessageCode *code1, DSMessageCode *code2) {
-  return [code1 isEqualToString:code2];
+  return [code1 isEqual:code2];
 }
